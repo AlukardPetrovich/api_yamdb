d37e58432747be9ac11390a1068ece0580b12ce1
Artem <naumov.a.p@gmil.com>
2022-02-20 11:33:09 +0300

    fix pep8 remarks

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -37,8 +37,8 @@ class IsAdminOrModeratorOrOwner(BasePermission):
 class IsAuthorOrAdminOrModeratorOrRead(BasePermission):
     def has_permission(self, request, view):
         return (
-                request.method in SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
